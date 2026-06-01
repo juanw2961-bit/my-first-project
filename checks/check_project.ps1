d5d475d2acb497fcd1f5b4fa701a176a0b9b20ff
juanw2961-bit <juanw2961@gmail.com>
2026-06-01 13:22:33 +0800

    Add checks/check_project.ps1

---
+++ checks/check_project.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = "Stop"
+
+$readme = Join-Path $PSScriptRoot "..\README.md"
+if (-not (Test-Path $readme)) {
+  throw "README.md is missing"
+}
+
+$content = Get-Content $readme -Raw
+if ($content -notmatch "我的第一个项目") {
+  throw "README.md must include the Chinese project title"
+}
+
+Write-Host "Project check passed"
